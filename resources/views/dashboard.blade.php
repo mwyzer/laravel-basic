b9a7620f29768eee99249b5f17e73a9d61a44e29
mwyzer <muhammad.wyzer@gmail.com>
2023-03-07 01:11:51 +0700

    change email into username in loginpage and register

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,11 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <div>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight mt-4">
+                {{ __('Dashboard') }}
+            </h2>
+        </div>
+
     </x-slot>
 
     <div class="py-12">
